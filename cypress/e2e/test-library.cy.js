02d3e7515a48bfd549f11ba9c3e99b85f75ccd76
Andrea Barrera <andrea.barrera7@gmail.com>
2023-11-27 19:42:39 -0500

    Creating tests for hover and navigation bar

--- cypress/e2e/test-library.cy.js
+++ cypress/e2e/test-library.cy.js
@@ -6,13 +6,17 @@ describe("Testing site for banrep library", () => {
     cy.get(".header-image").should("be.visible");
   });
   it("Must hover on Nosotros", () => {
-    cy.get(
-      '#menu-menu-1 > .menu-item-36074 > [href="https://pnliafi.com/que-es-iafi/"]'
-    ).trigger("mouseover");
+    cy.get("#menu-menu-1 > .menu-item-36074").trigger("mouseover");
   });
-  it("Must have 4 options on Nosotros", () => {
-    cy.get(
-      '#menu-menu-1 > .menu-item-36074 > [href="https://pnliafi.com/que-es-iafi/"]'
-    ).should("have.length", 1);
+  // it("Must have 5 options on Nosotros", () => {
+  //   cy.get('#menu-menu-1 > .menu-item-36074 > [href="https://pnliafi.com/que-es-iafi/"] > .dropdown-menu-toggle').should("have.length", 5);
+  // });
+  it("Must hover on Formacion", () => {
+    cy.get("#menu-menu-1 > .menu-item-34995 > :nth-child(1)").trigger(
+      "mouseover"
+    );
+  });
+  it("Must have a navigation bar", () => {
+    cy.get("#site-navigation > .inside-navigation").should("be.visible");
   });
 });
